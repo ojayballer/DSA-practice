bdb1b3d7e72e0febc7f2b1fb793e82816e8f1e4d
omojire <omojiremurewa@gmail.com>
2026-06-12 16:41:35 +0100

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        #n=str(n) apparently python would convert the binary num to dec
+        n =bin(n) # i have to explicitly sk it to leave it as a binary number 
+        count=0
+        for i in n :
+            if i == '1' :
+                count+=1
+
+        return  count
