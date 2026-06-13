ea93065b730ccac626eca800ddd6e7d9ea93d36c
omojire <omojiremurewa@gmail.com>
2026-06-13 07:43:38 +0100

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        hash= set()
+        i=0
+        for i in range(len(nums)+1) :
+            if i not in nums :
+                return i 
+                
+
