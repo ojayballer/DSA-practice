c334169bef49d3020a7caea425df822588e2d7ae
omojire <omojiremurewa@gmail.com>
2026-07-05 15:01:32 +0100

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        charSet = set(s)
+        res=0
+        for char in charSet : # O(1) look-up
+            l=0
+            count = 0 
+
+            for r in range(len(s)):
+                if  s[r]==char :
+                    count +=1 
+                
+                while (r-l+1)-count >k :#invalid window
+                    if s[l] == char :
+                        count-=1 
+                    l+=1
+                
+                res = max(res,r-l+1)
+        return res 
+
+
+
+        
