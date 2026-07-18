52ba9b2d63a73932bfbe9a24b38c7bc6d156e3fa
omojire <omojiremurewa@gmail.com>
2026-07-18 12:01:43 +0100

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if  len(edges) != n -1 : #first exit condition 
+            return False 
+
+        graph = {i:[] for i in range(n)}
+        
+        q=collections.deque()
+        for a,b in edges :
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        #let's start bfs from 0,alternatively we can start from any node since this graph is not directed
+        q.append(0)
+        visit=set()
+        while q :
+            node= q.popleft()
+            visit.add(node)
+            
+            for nei in graph[node]:
+                if nei in visit :
+                    continue 
+                visit.add(nei)
+                q.append(nei)
+        
+        for i in range(n):
+            if i not in visit :
+                return False 
+        return True 
+        #my logic : if every node in the graph  reach  any other nodes(even  if it is through their neighbours) then it is valid tree,which 
+        #means all the noded must be present in the visit set else it is not a valid graph 
+
+
+
+
+
+        
