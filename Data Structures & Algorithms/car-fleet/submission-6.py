f25e61681d205af233a2f061e99a631bbc010d0b
omojire <omojiremurewa@gmail.com>
2026-08-20 17:43:54 +0100

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+       
+       points = [(position,speed) for position,speed in zip(position,speed)]
+       points.sort(reverse=True) # sort by position in reverse from biggest to smallest 
+       
+       time =[(target -position)/speed for position ,speed in points]
+       stack=[]
+       r=0
+       stack.append(time[0])
+       r+=1
+       while r < len(position) : 
+          while stack and time[r] > stack[-1] :
+              stack.append(time[r])
+          r+=1 
+       return len(stack)
+          
+           
+
